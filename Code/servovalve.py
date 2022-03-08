aeb4facdc40439695603e99e1ab80d2ba0345314
gwyatt40 <55802319+gwyatt40@users.noreply.github.com>
2022-03-08 10:31:32 -0500

    changed pi 17 to 18 -G

--- Code/servovalve.py
+++ Code/servovalve.py
@@ -1,7 +1,7 @@
 from gpiozero import AngularServo
 from time import sleep
 
-servo =AngularServo(17, min_angle=0, max_angle=270, min_pulse_width=0.0005, max_pulse_width=0.0025)
+servo =AngularServo(18, min_angle=0, max_angle=270, min_pulse_width=0.0005, max_pulse_width=0.0025)
 
 while (True):
     servo.angle = 0
