c632c778b266d857ab7b840d0df13f1b34962097
gwyatt40 <55802319+gwyatt40@users.noreply.github.com>
2022-05-26 10:29:47 -0400

    updated and commented altimetervalve.py

--- Code/altimetervalve.py
+++ Code/altimetervalve.py
@@ -1,31 +1,38 @@
-import time
+import time # time libraries
 
-import board
+import board # board libraries 
 
-import adafruit_mpl3115a2
+import adafruit_mpl3115a2 # altimeter libraries 
 
-from gpiozero import AngularServo
-from time import sleep
+from gpiozero import AngularServo # angular servo libraries 
+from time import sleep # sleep function libraries 
 
-i2c = board.I2C()
+i2c = board.I2C() # sets I2c transaction 
 
 sensor = adafruit_mpl3115a2.MPL3115A2(i2c)
 
-sensor.sealevel_pressure = 102250
+sensor.sealevel_pressure = 102250 # sets sea level pressure (for Charlottesville, VA) 
 
-servo =AngularServo(18, min_angle=0, max_angle=270, min_pulse_width=0.0005, max_pulse_width=0.0025)
+servo1=AngularServo(18, min_angle=0, max_angle=180, min_pulse_width=0.0005, max_pulse_width=0.0025) # first servo, pin 18, controls valve
 
-while True:
+servo2=AngularServo(13, min_angle=0, max_angle=270, min_pulse_width=0.0005, max_pulse_width=0.0025) # second servo, pin 13, controls drop
                     
-     altitude = sensor.altitude
-     print("Altitude: {0:0.3f} meters".format(altitude))
+while True:
 
-     if altitude > 111:
-          servo.angle = 0
-          print("0")
+     altitude = sensor.altitude # reads sensor for altitude variable 
+
+     print("Altitude: {0:0.3f} meters".format(altitude)) # converts altitude to meters and prints 
+
+
+     if altitude > 104: # this value may change depending on the initial reading of the altimeter and the desired drop height 
+          servo1.angle =100
+          servo2.angle =100
+          print("100")
           sleep(2)
 
-     else:
-          servo.angle = 260
-          print ("260")
+
+     if altitude < 104:
+          servo1.angle =160 # servo angles can be adjusted depending on project requirements 
+          servo2.angle =160
+          print ("160")
           sleep(2)
